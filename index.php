d1f036fa271109d1c457481a24d2526bf274e775
AronneCarissimi <101709237+AronneCarissimi@users.noreply.github.com>
2024-05-09 09:47:02 +0000

    a

--- index.php
+++ index.php
@@ -3,9 +3,42 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Algebra linear</title>
+    <title>Algebra lineare</title>
+    <style>
+        table {
+            border-collapse: collapse;
+        }
+
+        table, th, td {
+            border: 1px solid black;
+        }
+        </style>
 </head>
+<script>
+    function creaTabella() {
+        var numero = document.getElementById("numero").value;
+        var tabella = document.getElementById("tabella");
+        let cose =    "<table><tr><th>dati</th><th>X(A)</th><th>X(B)</th></tr>";
+        if (numero > 0) {
+            for (let i = 0; i < numero; i++) {
+                cose += "<tr><td>dato " + (i+1) + "</td><td><input type='number'></td><td><input type='number'></td></tr>";
+            }
+            cose += "</table>";
+        tabella.innerHTML = cose;
+
+        }
+    }
+</script>
 <body>
-    aaaaaaaa
+    <div>
+    <label for="numero">inserisci il numero di righe</label>
+        <input type="number" id="numero" value=2>
+        <button onclick="creaTabella()">crea tabella</button>
+    </div>
+    <div id="tabella">
+
+    </div>
+
+
 </body>
 </html>
